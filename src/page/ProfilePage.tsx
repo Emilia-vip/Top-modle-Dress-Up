9f102ab2b9da0d1d0f7b8336be3bb3e2010ece7e
OhaaNana <Nanna-98--@hotmail.com>
2025-12-23 10:12:57 +0100

    Update ProfilePage

--- src/page/ProfilePage.tsx
+++ src/page/ProfilePage.tsx
@@ -6,6 +6,7 @@ import { AuthContext } from "../contexts/AuthContext";
 import apiClient from "../api/client";
 import type { Outfit } from "../type";
 import { tops, bottoms } from "../data/clothes";
+import axios from "axios";
 
 function ProfilePage() {
   const { user } = useContext(AuthContext);
