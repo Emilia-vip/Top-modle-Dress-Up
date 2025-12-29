a8168a9f0ae09dac38fbf82ce39d08dd3767f89c
OhaaNana <Nanna-98--@hotmail.com>
2025-12-29 15:20:32 +0100

    made som changes in clothes.tsx, assets and GamePage

--- src/data/clothes.tsx
+++ src/data/clothes.tsx
@@ -1,28 +1,39 @@
 // IMPORTERA BASDOCKOR
-import dollDarkUpper from '../assets/character/character-dark-upperbody.png';
-import dollDarkLower from '../assets/character/character-dark-lowerbody.png';
-import dollLightUpper from '../assets/character/character-light-upperbody.png';
-import dollLightLower from '../assets/character/character-light-lowerbody.png';
+//import dollDarkUpper from '../assets/character/character-dark-upperbody.png';
+import dollDarkUpper from '../assets/character/Character-upper-body.png';
+import dollDarkLower from '../assets/character/Character-lower-body.png';
+import dollLightUpper from '../assets/character/Light-doll-upper-body.png';
+import dollLightLower from '../assets/character/Light-doll-lower-body.png';
 
 // IMPORTERA KLÄDER 
-import top1dark from '../assets/darkskin/top/beige-troja.png';
-import top2dark from '../assets/darkskin/top/blueS-shirt.png';
-import top3dark from '../assets/darkskin/top/pink-t-shirt.png';
-import top4dark from '../assets/darkskin/top/stickad-tröjabeige.png';
-import bottom1dark from '../assets/darkskin/bottom/black-pants&Shoes.png';
-import bottom2dark from '../assets/darkskin/bottom/dark-jeans.png';
-import bottom3dark from '../assets/darkskin/bottom/jeans-beige-shoes.png';
-import bottom4dark from '../assets/darkskin/bottom/jeans-white-shoes.png';
+import top1dark from '../assets/darkskin/top/Beige-Blazer-Red-Top.png';
+import top2dark from '../assets/darkskin/top/Blue-Blazer-Beige-Top.png';
+import top3dark from '../assets/darkskin/top/Beige-cardigan.png';
+import top4dark from '../assets/darkskin/top/Beige-Top.png';
+import top5dark from '../assets/darkskin/top/Black-Top.png';
+import top6dark from '../assets/darkskin/top/Blue-Top.png';
+import top7dark from '../assets/darkskin/top/Red-Top.png';
+import bottom1dark from '../assets/darkskin/bottom/Black-SuitPants.png';
+import bottom2dark from '../assets/darkskin/bottom/Brown-SuitPants.png';
+import bottom3dark from '../assets/darkskin/bottom/Green-SuitPants.png';
+import bottom4dark from '../assets/darkskin/bottom/Beige-Skirt.png';
+import bottom5dark from '../assets/darkskin/bottom/Beige-skirt-belt.png';
 
 
-import top1light from '../assets/lightskin/top/beige-crop-top.png';
-import top2light from '../assets/lightskin/top/beige-sweatshirt.png';
-import top3light from '../assets/lightskin/top/blue-skirt.png';
-import top4light from '../assets/lightskin/top/pink-t-shirt.png';
-import bottom1light from '../assets/lightskin/bottom/black-pants.png';
-import bottom2light from '../assets/lightskin/bottom/dark-bootcut-jeans.png';
-import bottom3light from '../assets/lightskin/bottom/jeans.png';
-import bottom4light from '../assets/lightskin/bottom/jenas-blue.png';
+
+import top1light from '../assets/lightskin/top/Beige-Blazer-Red-Top.png';
+import top2light from '../assets/lightskin/top/Blue-Blazer-Beige-Top.png';
+import top3light from '../assets/lightskin/top/Beige-Cardigan.png';
+import top4light from '../assets/lightskin/top/Beige-Top.png';
+import top5light from '../assets/lightskin/top/Black-Top.png';
+import top6light from '../assets/lightskin/top/Blue-Top.png';
+import top7light from '../assets/lightskin/top/Red-Top.png';
+import bottom1light from '../assets/lightskin/bottom/Black-SuitPants.png';
+import bottom2light from '../assets/lightskin/bottom/Brown-SuitPants.png';
+import bottom3light from '../assets/lightskin/bottom/Green-SuitPants.png';
+import bottom4light from '../assets/lightskin/bottom/Beige-Skirt.png';
+import bottom5light from '../assets/lightskin/bottom/Beige-skirt-belt.png';
+import bottom6light from '../assets/lightskin/bottom/Black-Pants.png';
 
 
 const TOPSDARK = [
@@ -33,23 +44,38 @@ const TOPSDARK = [
     },
     {
         id: 'top-1dark',
-        name: 'beige shirt',
+        name: 'Beige blazer and red top',
         image: top1dark
     },
     {
         id: 'top-2dark',
-        name: 'blue shirt',
+        name: 'Blue blazer beige top',
         image: top2dark
     },
     {
         id: 'top-3dark',
-        name: 'pink t-shirt',
+        name: 'Beige cardigan',
         image: top3dark
     },
     {
         id: 'top-4dark',
-        name: 'beige knitted shirt',
+        name: 'Beige top',
         image: top4dark
+    },
+    {
+        id: 'top-5dark',
+        name: 'Black top',
+        image: top5dark
+    },
+    {
+        id: 'top-6dark',
+        name: 'Blue top',
+        image: top6dark
+    },
+    {
+        id: 'top-7dark',
+        name: 'Red top',
+        image: top7dark
     },
 
 ]
@@ -62,23 +88,28 @@ const BOTTOMSDARK = [
     },
     {
         id: 'bottom-1dark',
-        name: 'black pants',
+        name: 'Black suit pants wide',
         image: bottom1dark
     },
     {
         id: 'bottom-2dark',
-        name: 'dark jeans',
+        name: 'Brown suit pants',
         image: bottom2dark
     },
     {
         id: 'bottom-3dark',
-        name: 'jenas ',
+        name: 'Green suit pants',
         image: bottom3dark
     },
     {
         id: 'bottom-4dark',
-        name: 'jeans w shoes',
+        name: 'Beige skirt',
         image: bottom4dark
+    },
+    {
+        id: 'bottom-5dark',
+        name: 'Beige skirt with belt ',
+        image: bottom5dark
     },
 ]
 
@@ -91,23 +122,38 @@ const TOPSLIGHT = [
     },
     {
         id: 'top-1light',
-        name: 'crop top beige',
+        name: 'Beige blazer and red top',
         image: top1light
     },
     {
         id: 'top-2light',
-        name: 'swetshirt beige',
+        name: 'Blue blazer beige top',
         image: top2light
     },
     {
         id: 'top-3light',
-        name: 'shirt blue',
+        name: 'Beige cardigan',
         image: top3light
     },
     {
         id: 'top-4light',
-        name: 't-shirt pink',
+        name: 'Beige top',
         image: top4light
+    },
+    {
+        id: 'top-5light',
+        name: 'Black top',
+        image: top5light
+    },
+    {
+        id: 'top-6light',
+        name: 'Blue top',
+        image: top6light
+    },
+    {
+        id: 'top-7light',
+        name: 'Red top',
+        image: top7light
     },
 ]
 
@@ -119,23 +165,33 @@ const BOTTOMSLIGHT = [
     },
     {
         id: 'bottom-1light',
-        name: 'black pants',
+        name: 'Black suit pants wide',
         image: bottom1light
     },
     {
         id: 'bottom-2light',
-        name: 'jenas bootcut dark',
+        name: 'Brown suit pants',
         image: bottom2light
     },
     {
         id: 'bottom-3light',
-        name: 'light jeans',
+        name: 'Green suit pants',
         image: bottom3light
     },
     {
         id: 'bottom-4light',
-        name: 'jeans blue',
+        name: 'Beige skirt',
         image: bottom4light
+    },
+    {
+        id: 'bottom-5light',
+        name: 'Beige skirt with belt',
+        image: bottom5light
+    },
+    {
+        id: 'bottom-6light',
+        name: 'Black suit pants',
+        image: bottom6light
     },
     
 ]
