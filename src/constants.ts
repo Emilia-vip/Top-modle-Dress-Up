8c0e7cf373a863ab960c972a8a5ff1583be3bcbf
Emilia Eriksson <emiliaviperstrand@gmail.com>
2026-02-10 15:19:57 +0100

    fix the lockalhost

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // For development on computer: use localhost
 // For testing on phone: replace localhost with your computer's IP address (e.g., "http://192.168.1.100:3002")
 // To find your IP: Windows (ipconfig) or Mac/Linux (ifconfig)
-export const BASE_URL = "http://localhost:3002";
+export const BASE_URL = "http://localhost:3000";
 //export const BASE_URL = "https://api.leetcode.se/fos25-server";
 
 // For mobile testing, uncomment and replace with your computer's local IP:
