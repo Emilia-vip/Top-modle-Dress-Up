7ff8b1fcbb861f2a1f1e6d2bf2fbd27552d36a03
Emilia Eriksson <emiliaviperstrand@gmail.com>
2026-03-26 12:42:56 +0100

    I changes the base URL to the render one for deployment

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,8 @@
 // For development on computer: use localhost
 // For testing on phone: replace localhost with your computer's IP address (e.g., "http://192.168.1.100:3002")
 // To find your IP: Windows (ipconfig) or Mac/Linux (ifconfig)
-export const BASE_URL = "http://localhost:3002";
+//export const BASE_URL = "http://localhost:3002";
+ export const BASE_URL = "https://top-modle-dress-up-s1ca.onrender.com";
 //export const BASE_URL = "https://api.leetcode.se/fos25-server";
 
 // For mobile testing, uncomment and replace with your computer's local IP:
@@ -9,3 +10,4 @@ export const BASE_URL = "http://localhost:3002";
 
 export const REFRESH_TOKEN = "refreshToken";
 export const ACCESS_TOKEN = "accessToken";
+
